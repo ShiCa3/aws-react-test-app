2930f827a750ba6fbed188237cbab9896b847458
ShiCa3 <shikataro.6982.yamada@gmail.com>
2022-12-06 21:19:01 +0900

    Revert "add auth"
    This reverts commit 4129d9725717117abbcb36a4a0741cbde84f814f.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
-function App({signOut,user}) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 		<h2>Hello React with AWS</h2>
-		{user ? (
-			<>
-				<h3>私は権限を持ってます:{user.username}</h3>
-				<button onClick={signOut}>サインアウト</button>
-			</>
-		) : (
-			<h3>権限がありません</h3>
-		)}
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
