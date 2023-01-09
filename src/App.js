92ad992360a16860904274aa32c3b9c55d34cce0
ShiCa3 <shikataro.6982.yamada@gmail.com>
2023-01-09 23:00:47 +0900

    add Name and Message Function on Blockchain

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import abi from './BuyMeACoffee.json';
 import { ethers } from "ethers";
 import logo from './ShiCa2_face.svg';
 import './App.css';
-import {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4} from "uuid";
 
@@ -16,10 +16,18 @@ function App() {
 	const [errorMessage, setErrorMessage] = useState("");
 	const [name, setName] = useState("");
 	const [message, setMessage] = useState("");
+	const [memos, setMemos] = useState([]);
 
 	const [todos, setTodos] = useState([]);
-
 	const todoNameRef = useRef();
+
+	const onNameChange = (event) => {
+		setName(event.target.value);
+	}
+	
+	const onMessageChange = (event) => {
+		setMessage(event.target.value);
+	}
 
 	// メタマスクウォレット接続
 	const connectWallet = () => {
@@ -69,6 +77,32 @@ function App() {
 		}
 	};
 
+	// Function to fetch all memos stored on-chain.
+	const getMemos = async () => {
+	    try {
+	    	const { ethereum } = window;
+   		 	if (ethereum) {
+        		const provider = new ethers.providers.Web3Provider(ethereum);
+        		const signer = provider.getSigner();
+        		const buyMeACoffee = new ethers.Contract(
+        		contractAddress,
+        		contractABI,
+        		signer
+        	);
+        
+        	console.log("fetching memos from the blockchain..");
+        	const memos = await buyMeACoffee.getMemos();
+        	console.log("fetched!");
+        	setMemos(memos);
+      		} else {
+        		console.log("Metamask is not connected");
+      		}
+      
+    	} catch (error) {
+      	console.log(error);
+    	}
+	};
+
 	const handleAddTodo = () => {
 		//タスクを追加する
 		const name = todoNameRef.current.value;
@@ -91,6 +125,50 @@ function App() {
 		setTodos(newTodos);
 	}
 
+
+	useEffect(() => {
+		let buyMeACoffee;
+		// isWalletConnected();
+		getMemos();
+	
+		// Create an event handler function for when someone sends
+		// us a new memo.
+		const onNewMemo = (from, timestamp, name, message) => {
+		  console.log("Memo received: ", from, timestamp, name, message);
+		  setMemos((prevState) => [
+			...prevState,
+			{
+			  address: from,
+			  timestamp: new Date(timestamp * 1000),
+			  message,
+			  name
+			}
+		  ]);
+		};
+	
+		const {ethereum} = window;
+	
+		// Listen for new memo events.
+		if (ethereum) {
+		  const provider = new ethers.providers.Web3Provider(ethereum, "any");
+		  const signer = provider.getSigner();
+		  buyMeACoffee = new ethers.Contract(
+			contractAddress,
+			contractABI,
+			signer
+		  );
+	
+		  buyMeACoffee.on("NewMemo", onNewMemo);
+		}
+	
+		return () => {
+		  if (buyMeACoffee) {
+			buyMeACoffee.off("NewMemo", onNewMemo);
+		  }
+		}
+	}, []);
+
+
 	return(
 	    <div className="App">
     	  	<header className="App-header">
@@ -101,18 +179,35 @@ function App() {
 				<h1>シカくんにコーヒーを買ってあげる(Beta)</h1>
 				<div>
 					<label>お名前</label>
-					<input type="text" />
+					<input id="name" type="text" placeholder="anon" onChange={onNameChange} />
 				</div>
 				<div>
 					<label>メッセージ</label><br/>
        		        <textarea
                 	  rows={3}
                 	  placeholder="Enjoy your coffee!"
+					  id="message"
+					  onChange={onMessageChange}
+					  required 
                 	>
                 	</textarea>
 				</div>
 				<button onClick={connectWallet}>お財布接続(Metamask)</button>
 				<button onClick={buyCoffee}>コーヒー 0.001GoerliETH</button>
+
+				{
+				(memos.map((memo, idx) => {
+	        		return (
+			        	<div key={idx} style={{border:"2px solid", "border-radius":"5px", padding: "5px", margin: "5px"}}>
+            				<p style={{"font-weight":"bold"}}>"{memo.message}"</p>
+            				<p>
+								{memo.name}より
+								{/* {memo.timestamp.toString()} */}
+							</p>
+          				</div>
+        			)
+		      	}))}
+
 				<p>
 					シカくんにコーヒーを買ってあげる手順<br/>
 					1.chromeで<a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">Metamaskの拡張機能</a>をインストールする<br/>
@@ -121,7 +216,7 @@ function App() {
 					4.GoerliETHを入手する(<a href="https://goerlifaucet.com/">Goerli Faucet</a>)<br/>
 					5.お財布接続ボタンを押して、Metamaskと、このwebサイトを接続する<br/>
 					6.コーヒーのボタンを押す<br/>
-					7.お名前とメッセージはまだ反映されません<br/>
+					7.お名前とメッセージは、残らないかもしれません(テスト中)<br/>
 				</p>
 
 				<h1>シカくんToDoリスト</h1>
